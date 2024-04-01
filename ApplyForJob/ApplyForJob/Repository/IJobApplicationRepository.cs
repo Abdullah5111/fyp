ee3f205938c37465ede106ede06411f8b5c874be
Abdullah <122647095+Abdullah5111@users.noreply.github.com>
2024-04-01 20:40:18 +0500

    first commit

---
+++ ApplyForJob/ApplyForJob/Repository/IJobApplicationRepository.cs
@@ -0,0 +1,16 @@
+﻿using ApplyForJob.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ApplyForJob.Repository
+{
+    public interface IJobApplicationRepository
+    {
+        Task<IEnumerable<JobApplication>> GetAllJobApplications();
+        Task<JobApplication> GetJobApplicationById(int id);
+        Task<IEnumerable<JobApplication>> GetJobApplicationsByUsername(string username);
+        Task<JobApplication> AddJobApplication(JobApplication jobApplication);
+        Task<JobApplication> UpdateJobApplication(JobApplication jobApplication);
+        Task DeleteJobApplication(int id);
+    }
+}
